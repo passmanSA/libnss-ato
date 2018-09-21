68c99889861f3ac506e5e2bf967dd99b07cf72d5
mikomarrache <mickaelmarrache@gmail.com>
2018-09-21 15:56:05 +0300

    multiple changes
    - allow use of root user
    - only involve the plugin for sshd, login, unix_chkpwd and id processes

--- libnss_ato.c
+++ libnss_ato.c
@@ -1,4 +1,9 @@
 /*
+ * Modified by PassmanSA based on commits made on Metaswitch fork.
+ * https://github.com/Metaswitch/libnss-ato
+ */
+
+/*
  * libnss_ato.c
  *
  * Nss module to map every requested user to a fixed one.
@@ -26,10 +31,40 @@
 #include <shadow.h>
 #include <string.h>
 #include <stdio.h>
+#include <syslog.h>
+
+/*
+ * Match to the list of process names for which this module should return a
+ * result.
+ * They are processes which call nss_ato on login, and user-run OS command line
+ * programs.
+ */
+const char *VALID_PROC_NAMES[] = {"sshd",
+                                  "login",
+				  				  // SSHd on CentOS uses "unix_chkpwd", so ato
+				  				  // needs to respond to that
+                                  "unix_chkpwd",
+				  				  // The "id" program isn't necessary, but it's
+				  				  // useful for testing that the library is set
+				  				  // up correctly, and the README examples
+				  				  // don't work without it.
+                                  "id"};
+
+/*
+ * Array length macro
+ * The size of the whole array divided by the size of the 1st element is the
+ * length of the array
+ */
+#define arraylen(array) ((sizeof(array))/(sizeof(array[0])))
+
+#define FALSE 0
+#define TRUE 1
 
 /* for security reasons */
 #define MIN_UID_NUMBER   500
 #define MIN_GID_NUMBER   500
+#define ROOT_UID		 0
+#define ROOT_GID		 0
 #define CONF_FILE "/etc/libnss-ato.conf"
 
 /*
@@ -50,10 +85,10 @@ read_conf()
 	}
 	conf = fgetpwent(fd);
 
-	if ( conf->pw_uid < MIN_UID_NUMBER )
+	if ( conf->pw_uid < MIN_UID_NUMBER && conf->pw_uid != ROOT_UID )
 		conf->pw_uid = MIN_UID_NUMBER;
 
-	if ( conf->pw_gid < MIN_GID_NUMBER )
+	if ( conf->pw_gid < MIN_GID_NUMBER && conf->pw_gid != ROOT_GID )
 		conf->pw_gid = MIN_GID_NUMBER;
 
 	fclose(fd);
@@ -89,6 +124,57 @@ get_static(char **buffer, size_t *buflen, int len)
 	return result;
 }
 
+/*
+ * should_find_user
+ * This determines whether this module should return 'not found' for the user,
+ * based on the name of the process it's being called in.
+ * The module should return the user only it is being called as part of a
+ * login attempt (over ssh or the console) or by some OS command-line
+ * programs.
+ *
+ * This function returns a boolean.
+ */
+int
+should_find_user(void)
+{
+	FILE *fd;
+	/*
+	 * On Linux use the stat file to get the process name
+	 */
+	char proc_file[] = "/proc/self/stat";
+	int pid;
+
+	/* Open the file */
+	if ((fd = fopen(proc_file, "r")) == NULL )
+		return FALSE;
+
+	/* Read the process ID */
+	fscanf(fd, "%d ", &pid);
+
+	/*
+	* Read the process name, which is at most 16 characters and enclosed in
+	* brackets.
+	*/
+	char name[17];
+	fscanf(fd, "(%16[^)])", name);
+	fclose(fd);
+
+	/*
+	* Match to the list of permitted process names, defined at the top of the
+	* file.
+	*/
+	int i;
+	for (i = 0; i < arraylen(VALID_PROC_NAMES); i++) {
+		if (strcmp(name, VALID_PROC_NAMES[i]) == 0) {
+			syslog(LOG_AUTH|LOG_NOTICE, "libnss_ato: Process name matched '%s'", VALID_PROC_NAMES[i]);
+			return TRUE;
+		}
+	}
+
+	// Process name didn't match any that we want.
+	syslog(LOG_AUTH|LOG_NOTICE, "libnss_ato: libnss_ato is not set to return mapping to process '%s'", name);
+	return FALSE;
+}
 
 enum nss_status
 _nss_ato_getpwnam_r( const char *name, 
@@ -98,7 +184,12 @@ _nss_ato_getpwnam_r( const char *name,
 	             int *errnop)
 {
 	struct passwd *conf;
-  
+
+	if (!should_find_user()) {
+		syslog(LOG_AUTH|LOG_NOTICE, "libnss_ato: Not mapping user '%s' to default user", name);
+		return NSS_STATUS_NOTFOUND;
+	}
+
 	if ((conf = read_conf()) == NULL) {
 		return NSS_STATUS_NOTFOUND;
 	}
@@ -119,6 +210,7 @@ _nss_ato_getpwnam_r( const char *name,
 
 	strcpy(p->pw_passwd, "x");
 
+	syslog(LOG_AUTH|LOG_NOTICE, "libnss_ato: Mapping user '%s' to default user", name);
 	return NSS_STATUS_SUCCESS;
 }
 
@@ -129,24 +221,29 @@ _nss_ato_getspnam_r( const char *name,
                      size_t buflen,
                      int *errnop)
 {
+	if (!should_find_user()) {
+		syslog(LOG_AUTH|LOG_NOTICE, "libnss_ato: Not mapping user '%s' to default user", name);
+		return NSS_STATUS_NOTFOUND;
+	}
 
-        /* If out of memory */
-        if ((s->sp_namp = get_static(&buffer, &buflen, strlen(name) + 1)) == NULL) {
-                return NSS_STATUS_TRYAGAIN;
-        }
+	/* If out of memory */
+    if ((s->sp_namp = get_static(&buffer, &buflen, strlen(name) + 1)) == NULL) {
+        return NSS_STATUS_TRYAGAIN;
+    }
 
-        strcpy(s->sp_namp, name);
+    strcpy(s->sp_namp, name);
 
-        if ((s->sp_pwdp = get_static(&buffer, &buflen, strlen("*") + 1)) == NULL) {
-                return NSS_STATUS_TRYAGAIN;
-        }
+	if ((s->sp_pwdp = get_static(&buffer, &buflen, strlen("*") + 1)) == NULL) {
+    	return NSS_STATUS_TRYAGAIN;
+    }
 
-        strcpy(s->sp_pwdp, "*");
+    strcpy(s->sp_pwdp, "*");
 
-        s->sp_lstchg = 13571;
-        s->sp_min    = 0;
-        s->sp_max    = 99999;
-        s->sp_warn   = 7;
-
-        return NSS_STATUS_SUCCESS;
+    s->sp_lstchg = 13571;
+    s->sp_min    = 0;
+    s->sp_max    = 99999;
+    s->sp_warn   = 7;
+	
+	syslog(LOG_AUTH|LOG_NOTICE, "libnss_ato: Mapping user '%s' to default user", name);
+	return NSS_STATUS_SUCCESS;
 }
